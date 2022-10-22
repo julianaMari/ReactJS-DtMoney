3b405d36ac4d9b355dca8fc8c26475a19c53579f
Juliana Cruz <julianamariacruz.jm@gmail.com>
2022-10-21 21:39:08 -0300

    Installing styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import styled from 'styled-components'
+
+const Tytle = styled.h1`
+font-size: 64px;
+color: #8257e6;
+`
+
 export function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
+     <Tytle>
+      Hello
+      </Tytle>
     </div>
   );
 }
