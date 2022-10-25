6b1ce7a7526af32dd98d14705c914bac46043cc5
Juliana Cruz <julianamariacruz.jm@gmail.com>
2022-10-25 10:24:23 -0300

    Styling Modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,17 +1,49 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
+import fecharImg from '../../assets/fechar.svg';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
 }
 
-export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
     return (
         <Modal
             isOpen={isOpen}
             onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
         >
-            <h2>Cadastrar transação</h2>
+
+            <button 
+            type="button" 
+            onClick={onRequestClose} 
+            className="react-modal-close"
+            >
+                <img src={fecharImg} alt="Fechar modal" />
+            </button>
+
+            <Container>
+                <h2>Cadastrar transação</h2>
+
+                <input
+                    placeholder='Titulo'
+                />
+
+                <input
+                    type="number"
+                    placeholder='Valor'
+                />
+
+                <input
+                    placeholder='Categoria'
+                />
+
+                <button type="submit">
+                    Cadastrar
+                </button>
+            </Container>
         </Modal>
 
 
